c25e08bf1fb18c34a8655b82ec55c8c36e430213
Ridoy sheikh <111294017+ridoysheikh@users.noreply.github.com>
2023-02-23 00:47:55 +0600

    Update urls.py

--- Ridoy_Portfolio/urls.py
+++ Ridoy_Portfolio/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
- = [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Front_Pages.urls')),
     path('', include('dashboard.urls')),
