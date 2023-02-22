e7b0115b84ad2a99494342e4b68341599cc5054e
Ridoy sheikh <111294017+ridoysheikh@users.noreply.github.com>
2023-02-23 00:46:44 +0600

    Update urls.py

--- Ridoy_Portfolio/urls.py
+++ Ridoy_Portfolio/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+ = [
     path('admin/', admin.site.urls),
     path('', include('Front_Pages.urls')),
     path('', include('dashboard.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
